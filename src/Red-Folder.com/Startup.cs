9879d4f6aaaea2ddb04020127c1c481333017d35
Mark Taylor <mark@red-folder.com>
2016-03-20 20:46:05 +0000

    Bootstrapping the MVC and JavaScript dependancies

--- src/Red-Folder.com/Startup.cs
+++ src/Red-Folder.com/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Red_Folder.com
+namespace Red_Folder
 {
     public class Startup
     {
@@ -15,16 +15,21 @@ namespace Red_Folder.com
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
+            app.UseStaticFiles();
 
-            app.Run(async (context) =>
+            app.UseMvc(config =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" }
+                );
             });
         }
 
