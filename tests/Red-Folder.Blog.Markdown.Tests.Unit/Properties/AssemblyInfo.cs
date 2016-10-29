4bf03e9d2bb3ea75efeec2c53892bb25262c34aa
Mark Taylor <mark@red-folder.com>
2016-10-29 17:57:44 +0100

    Converted Red-Folder.Blog.Markdown.Tests.Unit to .Core library

--- tests/Red-Folder.Blog.Markdown.Tests.Unit/Properties/AssemblyInfo.cs
+++ tests/Red-Folder.Blog.Markdown.Tests.Unit/Properties/AssemblyInfo.cs
@@ -2,35 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Red-Folder.Blog.Markdown.Tests.Unit")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Red-Folder.Blog.Markdown.Tests.Unit")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Red_Folder.Blog.Markdown.Tests.Unit")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fdbdd62f-84ef-40c4-9170-68c61148f7a8")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("0a33e74c-8d79-4c5c-b001-c4ab094b3ea6")]
