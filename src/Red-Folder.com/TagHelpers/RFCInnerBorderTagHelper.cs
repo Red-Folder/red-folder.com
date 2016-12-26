0df11305119c30ff9bb7b0f5aea12cdb1065bc2b
Mark Taylor <mark@red-folder.com>
2016-12-26 14:25:22 +0000

    Added rfc-inner-border tag helper

---
+++ src/Red-Folder.com/TagHelpers/RFCInnerBorderTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace RedFolder.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "rfc-inner-border")]
+    public class RFCInnerBorderTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var childContent = output.Content.IsModified ? 
+                                output.Content.GetContent() :
+                                (await output.GetChildContentAsync()).GetContent();
+
+            output.Content.SetHtmlContent(@"<div class=""rfc-panel-inner"">" +
+                                            childContent +
+                                            "</div>");
+        }
+    }
+}
