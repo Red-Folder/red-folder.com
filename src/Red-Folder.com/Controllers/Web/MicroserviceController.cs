c8bdec599b52a0bd172cd1f8f7ff1c383486da99
Mark Taylor <mark@red-folder.com>
2017-02-12 13:14:56 +0000

    Converted Microservices page over new card format

--- src/Red-Folder.com/Controllers/Web/MicroserviceController.cs
+++ src/Red-Folder.com/Controllers/Web/MicroserviceController.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RedFolder.Services;
+using RedFolder.ViewModels;
 
 namespace RedFolder.Controllers.Web
 {
     public class MicroserviceController : Controller
     {
         // GET: MicroService
-        public ActionResult Index()
+        public ActionResult Index([FromServices] IBlogRepository repository)
         {
-            return View();
+            var blogs = repository.GetAll();
+            var collection = new BlogCollection(blogs, 1, 0, "MicroServices", BlogCollection.OrderBy.PublishedAscending);
+            return View(collection);
         }
     }
 }
