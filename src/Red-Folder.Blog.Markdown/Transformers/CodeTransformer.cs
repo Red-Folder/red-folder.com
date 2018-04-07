1b50c9d2ae3ce22913b8de55cd755898f501a5ea
Mark Taylor <markbryantaylor@gmail.com>
2018-04-07 15:55:59 +0100

    Improvements to how code in markdown is transformed

--- src/Red-Folder.Blog.Markdown/Transformers/CodeTransformer.cs
+++ src/Red-Folder.Blog.Markdown/Transformers/CodeTransformer.cs
@@ -15,10 +15,41 @@ namespace RedFolder.Blog.Markdown.Transformers
 
         }
 
-        protected override string PostTransform(JObject meta, string markdown)
+        protected override string PreTransform(JObject meta, string markdown)
         {
-            //return Regex.Replace(markdown, "<code>(.*)</code>", m => m.Value.Replace("\n", "<br/>\n"), RegexOptions.Singleline);
-            return markdown.Replace("<code>", "<pre><code>").Replace("</code>", "</code></pre>").Replace("<code>\n","<code>");
+            // Relacing Back Ticks (```) with <code> before it goes into Markdown avoids it seeing code as markdown
+            return ReplaceCodeElement(ReplaceBackTicks(markdown));
+        }
+
+        private string ReplaceBackTicks(string markdown)
+        {
+            var ticksReplacer = new TicksReplacer();
+            var ticksPattern = new Regex("```");
+            return ticksPattern.Replace(markdown, new MatchEvaluator(ticksReplacer.Replace));
+        }
+
+        private string ReplaceCodeElement(string markdown)
+        {
+            return markdown.Replace("<code>", "<pre><code>").Replace("</code>", "</code></pre>").Replace("<code>\n", "<code>");
+        }
+
+        private class TicksReplacer
+        {
+            private bool _isOpen = false;
+
+            public string Replace(Match m)
+            {
+                if (_isOpen)
+                {
+                    _isOpen = false;
+                    return "</code>";
+                }
+                else
+                {
+                    _isOpen = true;
+                    return "<code>";
+                }
+            }
         }
     }
 }
