fa8ed2237e02ee3285490617e214b8e20d8481d2
Red-Folder <markbryantaylor@gmail.com>
2019-06-20 17:22:57 +0100

    Added Podcasts page

---
+++ src/Red-Folder.com/Controllers/Web/PodcastsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CodeHollow.FeedReader;
+using System.Threading.Tasks;
+
+namespace RedFolder.Controllers.Web
+{
+    public class PodcastsController : Controller
+    {
+        public async  Task<ActionResult> Index()
+        {
+            var model = await FeedReader.ReadAsync("https://anchor.fm/s/b669760/podcast/rss");
+            return View(model);
+        }
+    }
+}
