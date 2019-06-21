0bd42a803bf7b829589abda09c409a0db73cfc98
Red-Folder <markbryantaylor@gmail.com>
2019-06-21 16:53:18 +0100

    Added simple podcast page

--- src/Red-Folder.com/Controllers/Web/PodcastsController.cs
+++ src/Red-Folder.com/Controllers/Web/PodcastsController.cs
@@ -1,15 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using CodeHollow.FeedReader;
 using System.Threading.Tasks;
+using System.Linq;
+using RedFolder.Utils;
+using CodeHollow.FeedReader.Feeds;
 
 namespace RedFolder.Controllers.Web
 {
     public class PodcastsController : Controller
     {
-        public async  Task<ActionResult> Index()
+        public async Task<ActionResult> Index()
         {
             var model = await FeedReader.ReadAsync("https://anchor.fm/s/b669760/podcast/rss");
             return View(model);
+        }
+
+        public async Task<ActionResult> Podcast(string id = "Episode-0--Why-Im-doing-this-podcast")
+        {
+            var feed = await FeedReader.ReadAsync("https://anchor.fm/s/b669760/podcast/rss");
+
+            var item = feed?.Items?.Single(x => SafeUrl.MakeSafe(x.Title) == id);
+            if (item == null)
+            {
+                return new RedirectResult("/errors/status/404");
+            }
+
+            return View(item);
         }
     }
 }
---
+++ src/Red-Folder.com/Utils/SafeUrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RedFolder.Utils
+{
+    public static class SafeUrl
+    {
+        private const string ALPHA_LOWER = "abcdefghijklmnopqrstuvwxyz";
+        private const string ALPHA_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string NUMERIC = "0123456789";
+        private const string SPACE = " ";
+
+        public static string MakeSafe(string source)
+        {
+            var pattern = string.Concat(ALPHA_LOWER, ALPHA_UPPER, NUMERIC, SPACE);
+            var cleaned = Regex.Replace(source, $"[^{pattern}]", "");
+
+            return cleaned.Replace(" ", "-");
+        }
+    }
+}
