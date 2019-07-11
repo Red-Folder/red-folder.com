2dd384229f0be4d811154793556fe16b8f1b3332
Red-Folder <markbryantaylor@gmail.com>
2019-07-11 12:35:37 +0100

    Added books ... and made some design tweaks to the activity page.  Needs a fair level of clean up

--- src/Red-Folder.com/Services/ActivityRepository.cs
+++ src/Red-Folder.com/Services/ActivityRepository.cs
@@ -17,7 +17,28 @@ namespace Red_Folder.com.Services
 
         public Weekly Weekly(string year, string weekNumber)
         {
-            return _client.Weekly(year, weekNumber);
+            var week = _client.Weekly(year, weekNumber);
+
+            week.Books = new BookActivity();
+            week.Books.Books = new List<Book>
+            {
+                new Book
+                {
+                    Title = "War and Peace and IT: Business Leadership, Technology, and Success in the Digital Age",
+                    Author = "Mark Schwartz",
+                    Description = "Advice for business leaders looking to lead their company into the digital age by harnessing the expertise and innovation that is already under their roof.",
+                    ImageUrl = "https://images-eu.ssl-images-amazon.com/images/I/41i6bnKdwUL.jpg"
+                },
+                new Book
+                {
+                    Title = "The Riftwar Saga Trilogy",
+                    Author = "Raymond E. Feist",
+                    Description = "Fantasy novel follow Pug and Tomas as suddenly the peace of the Kingdom is destroyed as mysterious alien invaders swarm the land",
+                    ImageUrl = "https://images-eu.ssl-images-amazon.com/images/I/41fNqtayNWL.jpg"
+                },
+            };
+
+            return week;
         }
     }
 }
