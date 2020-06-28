67345950fcbbc689642d20f64ce73e50476d942f
Mark Taylor <markbryantaylor@gmail.com>
2020-06-28 15:01:23 +0100

    Converted Home/Index over to inline CSS Styles

---
+++ src/Red-Folder.com/TagHelpers/RFCInlineStyleTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace RedFolder.TagHelpers
+{
+    [HtmlTargetElement("rfc-inline-style")]
+    public class RFCInlineStyleTagHelper : TagHelper
+    {
+        private readonly string _wwwRootPath;
+
+        public RFCInlineStyleTagHelper(IHostingEnvironment environment)
+        {
+            _wwwRootPath = environment.WebRootPath;
+        }
+
+        [HtmlAttributeName("css-file")]
+        public string CssFile { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!string.IsNullOrEmpty(CssFile))
+            {
+                var path = Path.Combine(_wwwRootPath, "css", CssFile);
+                if (File.Exists(path))
+                {
+                    var content = await File.ReadAllTextAsync(path);
+                    output.Content.SetHtmlContent($"<style>{content}</style>");
+                }
+            }
+        }
+    }
+}
