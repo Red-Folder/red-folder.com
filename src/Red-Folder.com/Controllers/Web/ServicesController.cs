47f0ab6dac8a7302876eedcaff740431a85f3e50
Mark Taylor <markbryantaylor@gmail.com>
2020-07-22 14:56:29 +0100

    Rewrite of the Services page

--- src/Red-Folder.com/Controllers/Web/ServicesController.cs
+++ src/Red-Folder.com/Controllers/Web/ServicesController.cs
@@ -1,8 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RedFolder.ViewModels;
+using System.Collections.Generic;
 
 namespace RedFolder.Controllers.Web
 {
@@ -10,13 +8,58 @@ namespace RedFolder.Controllers.Web
     {
         public IActionResult Index()
         {
-            return View();
+            var services = new List<Service>
+            {
+                new Service
+                {
+                    Title = "Leadership",
+                    Description = new List<string>
+                    {
+                        "15+ years management experience",
+                        "Team Lead to Head of IT",
+                        "Scrum Master Certified",
+                        "Agile/ Scrum coaching/ evangelism",
+                        "Specialist in improving legacy teams"
+                    }
+                },
+                new Service
+                {
+                    Title = "Architecture",
+                    Description = new List<string>
+                    {
+                        "Architecting Microsoft Azure Solutions Certified",
+                        "Considerable Solution Architecture experience",
+                        "Patterns: Microservices, Web Application, Distributed",
+                        "Technical mentoring and leadership",
+                        "Reverse engineering legacy systems"
+                    }
+                },
+                new Service
+                {
+                    Title = "Development",
+                    Description = new List<string>
+                    {
+                        "20+ years development experience",
+                        "Microsoft App Builder/ Web Applications Certified",
+                        "ASP.Net, MVC, C#, .Net Core. WebAPI",
+                        "React, Redux, Javascript, HTML, CSS",
+                        "Specialist in handling legacy systems"
+                    }
+                },
+                new Service
+                {
+                    Title = "DevOps",
+                    Description = new List<string>
+                    {
+                        "Managed various Microsoft, Linux & Unix environments",
+                        "Cloud Management – Microsoft Azure & Amazon AWS",
+                        "CI/ CD tool - Team City, Jenkins, VSTS, Octopus",
+                        "Database tools - SQL Server, MySQL, No SQL",
+                        "Filling the gaps between technical teams"
+                    }
+                },
+            };
+            return View(services);
         }
-
-        public IActionResult HealthCheck()
-        {
-            return View();
-        }
-
     }
 }
---
+++ src/Red-Folder.com/ViewModels/Service.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace RedFolder.ViewModels
+{
+    public class Service
+    {
+        public string Title { get; set; }
+        public List<string> Description { get; set; }
+    }
+}
