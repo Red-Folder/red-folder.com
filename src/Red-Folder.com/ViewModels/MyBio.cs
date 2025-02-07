42a65647ca5860fd3f56fabb18324629fe324006
Mark Taylor <mark@red-folder.com>
2025-02-07 17:56:03 +0000

    Moved certifications to ViewModel

---
+++ src/Red-Folder.com/ViewModels/MyBio.cs
@@ -0,0 +1,10 @@
+﻿using RedFolder.ViewModels;
+
+namespace RedFolder.ViewModels
+{
+    public class MyBio
+    {
+        public Employment[] Employment { get; set; }
+        public Certification[] Certifications { get; set; }
+    }
+}
