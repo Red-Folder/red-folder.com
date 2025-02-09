b1be05dbff9821753fde6bd0816b1606be0ec9f4
Mark Taylor <mark@red-folder.com>
2025-02-09 16:22:03 +0000

    Additional saftey tests

--- tests/RedFolder.WebSite.Integration.Tests/HomePage.Tests.cs
+++ tests/RedFolder.WebSite.Integration.Tests/HomePage.Tests.cs
@@ -60,5 +60,23 @@ namespace RedFolder.WebSite.Integration.Tests
             settings.ScrubLinesContaining("<a id=\"cookie-consent-acceptance\"");
             await Verifier.Verify(formatted, settings).UseDirectory("Snapshots");
         }
+
+        [Fact]
+        public async Task Get_Redirect_RedirectsForKnownRoutes()
+        {
+            var response = await _httpClientFixture.Client.GetAsync("/redirect?url=http://blog.red-folder.com/2016/09/rfc-weekly-12th-september-2016.html");
+
+            Assert.Equal(System.Net.HttpStatusCode.MovedPermanently, response.StatusCode);
+            Assert.Contains("/blog/rfc-weekly-12th-september-2016", response.Headers.Location.ToString());
+        }
+
+        [Fact]
+        public async Task Get_Redirect_RedirectsToNotFoundForUnknownRoutes()
+        {
+            var response = await _httpClientFixture.Client.GetAsync("/redirect?url=something-random");
+
+            Assert.Equal(System.Net.HttpStatusCode.Found, response.StatusCode);
+            Assert.Contains("/errors/status/404", response.Headers.Location.ToString());
+        }
     }
 }
---
+++ tests/RedFolder.WebSite.Integration.Tests/SiteMap.Tests.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using VerifyTests;
+using VerifyXunit;
+using Xunit;
+
+namespace RedFolder.WebSite.Integration.Tests
+{
+    [Collection("HttpClient collection")]
+    [UsesVerify]
+    public class SiteMapTests
+    {
+        private readonly HttpClientFixture _httpClientFixture;
+
+        public SiteMapTests(HttpClientFixture httpClientFixture)
+        {
+            _httpClientFixture = httpClientFixture;
+        }
+
+        [Fact]
+        public async Task Get_SiteMap_ReturnsSuccessAndCorrectContent()
+        {
+            var response = await _httpClientFixture.Client.GetAsync("/sitemap.xml");
+            response.EnsureSuccessStatusCode();
+
+            var raw = await response.Content.ReadAsStringAsync();
+            var formatted = Utils.XmlFormatter.FormatXml (raw);
+
+            var settings = new VerifySettings();
+            await Verifier.Verify(formatted, settings).UseDirectory("Snapshots");
+        }
+    }
+}
