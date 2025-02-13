ae25ab74a45075586f129099a528930770423749
Mark Taylor <mark@red-folder.com>
2025-02-13 12:04:34 +0000

    Fixes for the encoding issue

--- src/Red-Folder.com/Controllers/Web/ServicesController.cs
+++ src/Red-Folder.com/Controllers/Web/ServicesController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using RedFolder.ViewModels;
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace RedFolder.Controllers.Web
 {
@@ -8,57 +10,9 @@ namespace RedFolder.Controllers.Web
     {
         public IActionResult Index()
         {
-            var services = new List<Service>
-            {
-                new Service
-                {
-                    Title = "Leadership",
-                    Description = new List<string>
-                    {
-                        "15+ years management experience",
-                        "Team Lead to Head of IT",
-                        "Scrum Master Certified",
-                        "Agile/ Scrum coaching/ evangelism",
-                        "Specialist in improving legacy teams"
-                    }
-                },
-                new Service
-                {
-                    Title = "Architecture",
-                    Description = new List<string>
-                    {
-                        "Architecting Microsoft Azure Solutions Certified",
-                        "Considerable Solution Architecture experience",
-                        "Patterns: Microservices, Web Application, Distributed",
-                        "Technical mentoring and leadership",
-                        "Reverse engineering legacy systems"
-                    }
-                },
-                new Service
-                {
-                    Title = "Development",
-                    Description = new List<string>
-                    {
-                        "20+ years development experience",
-                        "Microsoft App Builder/ Web Applications Certified",
-                        "ASP.Net, MVC, C#, .Net Core. WebAPI",
-                        "React, Redux, Javascript, HTML, CSS",
-                        "Specialist in handling legacy systems"
-                    }
-                },
-                new Service
-                {
-                    Title = "DevOps",
-                    Description = new List<string>
-                    {
-                        "Managed various Microsoft, Linux & Unix environments",
-                        "Cloud Management – Microsoft Azure & Amazon AWS",
-                        "CI/ CD tool - Team City, Jenkins, VSTS, Octopus",
-                        "Database tools - SQL Server, MySQL, No SQL",
-                        "Filling the gaps between technical teams"
-                    }
-                },
-            };
+            var servicesFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "services.json");
+            var servicesJson = System.IO.File.ReadAllText(servicesFilePath);
+            var services = JsonConvert.DeserializeObject<List<Service>>(servicesJson);
             return View(services);
         }
     }
